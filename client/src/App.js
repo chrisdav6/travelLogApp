0934bf53d065b28e968cbf2cc5574226ac2a1b6f
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-04-14 11:55:02 -0400

    Configured react-map-gl

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 39.8283459,
+    longitude: -98.5794797,
+    zoom: 4.5
+  });
+
+  return <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} onViewportChange={setViewport} />;
+};
 
 export default App;
